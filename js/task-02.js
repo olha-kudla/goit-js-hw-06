472d61b4933848d936f97152b2822697b9cf6c95
olha-kudla <119434247+olha-kudla@users.noreply.github.com>
2023-04-22 19:35:41 +0300

    update task #2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const refIngredients = document.querySelector('#ingredients');
+
+const items = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add('item');
+  return item;
+})
+
+refIngredients.append(...items);
+
+
