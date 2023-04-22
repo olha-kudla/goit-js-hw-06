707dec03517d5a397095a96185d6d26639b7f3e1
olha-kudla <119434247+olha-kudla@users.noreply.github.com>
2023-04-23 01:05:19 +0300

    update task #5

---
+++ js/task-05.js
@@ -0,0 +1,16 @@
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input 
+// (подія input), підставляє його поточне значення в span#name-output. 
+// Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+
+
+
+const inputRef = document.querySelector("#name-input");
+const outputRef = document.querySelector("#name-output");
+
+inputRef.addEventListener("input", event => {
+    outputRef.textContent = event.currentTarget.value;
+
+    if (event.currentTarget.value === "") {
+        outputRef.textContent = "Anonymous"
+    }
+})
