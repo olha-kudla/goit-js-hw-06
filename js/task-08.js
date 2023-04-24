362cd8a5352c97fd2e1e3af4942a7bd816e3ae1c
olha-kudla <119434247+olha-kudla@users.noreply.github.com>
2023-04-24 11:14:04 +0300

    update task #8

--- js/task-08.js
+++ js/task-08.js
@@ -18,16 +18,25 @@
 
 const form = document.querySelector(".login-form");
 
+const emailValue = '';
+const passwordValue ='';
+
 const handleSubmit = (event) => {
     event.preventDefault();
     const { elements: { email, password }
     } = event.currentTarget;
 
     if (email.value === "" || password.value === "") {
-        return console.log("Please fill in all the fields!");
+        return alert("Please fill in all the fields!");
     }
 
-    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    console.log({
+        Email: email.value,
+        Password: password.value
+    });
+
+    // console.log('Email: ${email.value}, Password: ${password.value');
+
     event.currentTarget.reset();
 }
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,26 +1,42 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
 
 
 // Напиши скрипт, який змінює кольори фону елемента <body> через 
 // інлайн-стиль по кліку 
 // на button.change-color і виводить значення кольору в span.color.
 
-const btnRef = document.querySelector(".change-color");
-console.log(btnRef);
+// const btnRef = document.querySelector(".change-color");
+// console.log(btnRef);
 
-const colorSpanRef = document.querySelector(".color");
-console.log(colorSpanRef);
+// const colorSpanRef = document.querySelector(".color");
+// console.log(colorSpanRef);
 
-const bodyRef = document.querySelector("body");
-console.log(bodyRef);
+// const bodyRef = document.querySelector("body");
+// console.log(bodyRef);
 
-const changeColor = (event) => {
-  bodyRef.style.backgroundColor = getRandomHexColor();
-  colorSpanRef.textContent = bodyRef.style.backgroundColor;
-}
+// const changeColor = (event) => {
+//   bodyRef.style.backgroundColor = getRandomHexColor();
+//   colorSpanRef.textContent = bodyRef.style.backgroundColor;
+// }
 
-btnRef.addEventListener("click", changeColor);
+// btnRef.addEventListener("click", changeColor);
+
+
+const myArray = [1, 36, 'Olhqa', false, 3, 4, 5];
+console.log(myArray);
+
+myArray.push('kit');
+console.log(myArray);
+
+const removeElement = myArray.pop();
+console.log(removeElement);
+
+myArray.unshift('polokoloikbhcdhbcdxhbj');
+console.log(myArray);
+
+const removeShiftElement = myArray.shift();
+console.log(removeShiftElement);
